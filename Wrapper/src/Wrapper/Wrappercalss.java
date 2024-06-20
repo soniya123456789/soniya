c43e4e9a7508a9f7cebcc02a3d0039c690550fca
soniya123456789 <150126683+soniya123456789@users.noreply.github.com>
2024-06-20 22:45:51 +0530

    Add files via upload

---
+++ Wrapper/src/Wrapper/Wrappercalss.java
@@ -0,0 +1,13 @@
+package Wrapper;
+
+public class Wrappercalss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int k=31,n;
+		Integer j= new Integer(12);
+		j=k;
+
+	}
+
+}
