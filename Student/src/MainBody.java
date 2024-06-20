8a084de7f84d63c324f57704c200b9a17a7e03a1
soniya123456789 <150126683+soniya123456789@users.noreply.github.com>
2024-06-20 22:44:01 +0530

    Add files via upload

---
+++ Student/src/MainBody.java
@@ -0,0 +1,12 @@
+
+public class MainBody {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Student s1=new Student();
+		 System.out.println("Student 1: " + s1.getName());
+		 Student s2=new Student("siya");
+		 System.out.println("Student 2: " + s2.getName());
+	}
+
+}
---
+++ Student/src/Student.java
@@ -0,0 +1,16 @@
+
+public class Student {
+	String sname;
+	public Student() {
+		sname="Unknown";
+	}
+	public Student(String sname) {
+		
+		this.sname = sname;
+	}
+	
+	public String getName() {
+        return sname;
+    }
+
+}
